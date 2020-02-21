3d8190f61ae393ee9a77a816d9e0820e9332ae79
basyusuf <y.penava@hotmail.com>
2020-02-22 02:58:23 +0300

    Change font

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/root/App/App';
 import 'semantic-ui-css/semantic.min.css';
 /* Font Css */
-import './fonts/montserrat/montserrat.css';
+import './fonts/FontFile.css';
 /* Font Css */
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
