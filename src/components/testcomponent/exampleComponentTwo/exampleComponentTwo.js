11110f7ea35ddb75073ec9a4cb2ce0afb3a23900
basyusuf <y.penava@hotmail.com>
2020-02-22 02:04:09 +0300

    First Commit

---
+++ src/components/testcomponent/exampleComponentTwo/exampleComponentTwo.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ExampleComponentTwo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Example Component Two</h1>
+            </div>
+        );
+    }
+}
+
+export default ExampleComponentTwo;
